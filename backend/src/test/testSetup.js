f35aaca4ef7b9099a9f48c2caf7dc9f1e60a998f
santibozzo <santiagobozzo95@gmail.com>
2019-09-25 23:22:37 -0300

    add tests for get:users/{dni}

--- backend/src/test/testSetup.js
+++ backend/src/test/testSetup.js
@@ -1,26 +1,12 @@
 
 const config = require('../resources/config');
 const dataBase = config.dataBase;
+const jwt = require('jsonwebtoken');
+const requestLimitsModel = require('../models/requestLimitsModel');
 const mongoose = require('mongoose');
 const dataBaseInitializer = require('../dataAccess/dataBaseInitializer');
 
-
-// before(done => {
-// 	const connectionOptions = {
-// 		useNewUrlParser: true,
-// 		useUnifiedTopology: true,
-// 		useCreateIndex: true
-// 	};
-// 	mongoose.Promise = global.Promise;
-// 	mongoose.connect(`mongodb://${dataBase.host}:${dataBase.port}/${dataBase.name}Test`, connectionOptions)
-// 		.then(() => {
-// 			console.log('Connected to test DB');
-// 			dataBaseInitializer.initializeDataBase()
-// 				.then(result => done())
-// 				.catch(error => done(error));
-// 		})
-// 		.catch(error => done(error));
-// });
+/* ---- DB Initializer ---- */
 
 const connectionOptions = {
 	useNewUrlParser: true,
@@ -36,3 +22,20 @@ mongoose.connect(`mongodb://${dataBase.host}:${dataBase.port}/${dataBase.name}Te
 			.catch(error => console.error(error));
 	})
 	.catch(error => console.error(error));
+
+/* ---- Token generator ---- */
+
+exports.generateTokenForTests = (dni, limit) => {
+	return new Promise((resolve, reject) => generateTokenForTests(dni, limit, resolve, reject));
+};
+
+exports.generateExpiredToken = dni => {
+	return jwt.sign({dni}, config.tokenSecretKey, {expiresIn: '0h'});
+};
+
+function generateTokenForTests(dni, limit, resolve, reject) {
+	const token = jwt.sign({dni}, config.tokenSecretKey, {expiresIn: '1h'});
+	requestLimitsModel.updateRequestLimit(dni, {limit, uses: 0})
+		.then(result => resolve && resolve(token))
+		.catch(error => reject && reject(error));
+}
--- backend/src/test/usersTest.js
+++ backend/src/test/usersTest.js
@@ -1,7 +1,7 @@
 const BASE_PATH = '/api';
 
+const testSetup = require('./testSetup');
 const usersRouter = require('../routers/usersRouter');
-const loginRouter = require('../routers/loginRouter');
 const request = require('supertest');
 const assert = require('assert');
 const express = require('express');
@@ -10,32 +10,72 @@ const globals = {};
 
 app.use(express.json());
 app.use(`${BASE_PATH}/users`, usersRouter);
-app.use(`${BASE_PATH}/login`, loginRouter);
 
-describe('GET:/users/{id}', () => {
+describe('Users tests', () => {
 	before(done => {
-		request(app)
-			.post(`${BASE_PATH}/login`)
-			.send({dni: 50000000, password: '123456'})
-			.expect(200)
-			.then(response => {
-				globals['token'] = response.body.token;
+		globals['expiredToken'] = testSetup.generateExpiredToken(50000000);
+		testSetup.generateTokenForTests(50000000, 200)
+			.then(token => {
+				globals['token'] = token;
 				done();
 			})
 			.catch(error => done(error));
 	});
 
-	it('should return user 50000000', done => {
-		request(app)
-			.get(`${BASE_PATH}/users/50000000`)
-			.set('authorization', globals.token)
-			.expect(200)
-			.then(response => {
-				const user = response.body;
-				assert.strictEqual(user.dni, 50000000);
-				assert.strictEqual(user.email, '50000000@tpfullstack.com');
-				done();
-			})
-			.catch(error => done(error));
+	describe('GET:users/{id}', () => {
+		it('gets existing user', done => {
+			request(app)
+				.get(`${BASE_PATH}/users/50000000`)
+				.set('authorization', globals.token)
+				.expect(200)
+				.then(response => {
+					const user = response.body;
+					assert.strictEqual(user.dni, 50000000);
+					assert.strictEqual(user.email, '50000000@tpfullstack.com');
+					done();
+				})
+				.catch(error => done(error));
+		});
+
+		it('tries to get non-existing user', () => {
+			return request(app)
+				.get(`${BASE_PATH}/users/00000000`)
+				.set('authorization', globals.token)
+				.expect(404);
+		});
+
+		it('invalid dni length', () => {
+			return request(app)
+				.get(`${BASE_PATH}/users/50`)
+				.set('authorization', globals.token)
+				.expect(404);
+		});
+
+		it('invalid dni format', () => {
+			return request(app)
+				.get(`${BASE_PATH}/users/50asd`)
+				.set('authorization', globals.token)
+				.expect(400);
+		});
+
+		it('doesn\'t supply token', () => {
+			return request(app)
+				.get(`${BASE_PATH}/users/50000000`)
+				.expect(401);
+		});
+
+		it('supplies invalid token', () => {
+			return request(app)
+				.get(`${BASE_PATH}/users/50000000`)
+				.set('authorization', 'invalidToken')
+				.expect(401);
+		});
+
+		it('supplies expired token', () => {
+			return request(app)
+				.get(`${BASE_PATH}/users/50000000`)
+				.set('authorization', globals.expiredToken)
+				.expect(401);
+		});
 	});
 });
